2e081550455919d1b4948a1bdad5df7d86b720b1
wayneb97 <w_l_b@outlook.com>
2025-07-24 18:02:47 +0100

    Delete components directory

--- components/Header.js
+++
@@ -1,7 +0,0 @@
-export default function Header({ title }) {
-  return (
-    <header className='bg-gray-900 text-white p-4'>
-      <h1 className='text-xl font-semibold'>{title}</h1>
-    </header>
-  );
-}
